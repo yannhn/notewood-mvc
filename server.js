ceca968bc8e06601e088ae1c11b4ca1c43196dec
Yannik Hangst <yanso1@posteo.de>
2023-03-31 20:37:34 +0200

    setup server with express

---
+++ server.js
@@ -0,0 +1,8 @@
+// install express
+const express = require("epxress");
+const app = express();
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
