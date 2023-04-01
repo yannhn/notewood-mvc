a0fc8a6182c0f700073fb0a9fea925e8c650f26c
Yannik Hangst <yanso1@posteo.de>
2023-04-01 17:02:57 +0200

    connect to database

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 // install express
 const express = require("express");
+const connectDB = require("./config/database");
 const app = express();
 const PORT = 8001;
+
+require("dotenv").config({ path: "./config/.env" });
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
