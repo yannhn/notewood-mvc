2de65902bc620a930c7656265eb6edcbbd8c867c
Yannik Hangst <yanso1@posteo.de>
2023-05-05 15:37:43 +0200

    deployment test

--- config/database.js
+++ config/database.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_CONNECTION);
+    const conn = await mongoose.connect(process.env.DB_CONNECTION, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`MongoDB connected ${conn.connection.host}`);
   } catch (err) {
     console.log(err);
